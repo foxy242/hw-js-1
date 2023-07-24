f0d9348819fd5d590ac196cc7a18f79622231841
foxy242 <114094487+foxy242@users.noreply.github.com>
2023-07-24 21:24:16 +0300

    1

---
+++ js/nay.js
@@ -0,0 +1,44 @@
+
+const message = 'some string at lesson for test';
+
+console.log(message.length)
+
+console.log(message.toUpperCase())
+
+const a = 3;
+
+console.log(a)
+
+const are = 12;
+
+let city = "lviv";
+
+console.log(city)
+
+city = 'kyiv' ;
+
+console.log(city)
+
+city = 'kharkiv' ;
+
+console.log(city)
+
+var fistName = 'kiril';
+
+// 0 || '12' || null || true;
+
+// console.log(0 ||'7' || null || true)
+
+// true && true && false && true
+
+// 0 || 100 || '' || '?'
+
+// console.log(0 || null || '?' || '')
+
+// '' || 'poly' || 'vova' || '123';
+
+// console.log('' || 'poly' || 'vova' || '123')
+
+// 'mango' || false || 'vova' || true
+// true || false || true || true
+// console.log()
